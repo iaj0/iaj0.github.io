5b6b47e19e3abbd6d17008536f0df1a55ea3465a
Iram <iram@live.com.sg>
2023-05-04 18:47:19 -0400

    sync1

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,8 +3,11 @@ import "./style.css"
 import gsap from "gsap"
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
+
 //Scene
 const scene = new THREE.Scene()
+
+
 
 
 //Create sphere
@@ -16,11 +19,13 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
+
 //sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
+
 
 //Light
 const light = new THREE.PointLight(0xffffff, 1 , 100)
@@ -29,10 +34,14 @@ light.intensity = 1.25
 scene.add(light)
 
 
+
+
 //Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
 camera.position.z = 20
 scene.add(camera)
+
+
 
 
 //Renderer
@@ -42,6 +51,7 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(2)
 renderer.render(scene, camera)
 
+
 //Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
@@ -50,7 +60,8 @@ controls.enableZoom = false
 controls.autoRotate = true
 controls.autoRotateSpeed = 5
 
-//Resize 
+
+//Resize
 window.addEventListener('resize', () => {
   //Update sizes
   console.log(window.innerWidth)
@@ -63,6 +74,7 @@ window.addEventListener('resize', () => {
   renderer.setSize(sizes.width, sizes.height)
 })
 
+
 const loop = () => {
   controls.update()
   renderer.render(scene, camera);
@@ -70,11 +82,13 @@ const loop = () => {
 }
 loop();
 
+
 //Timeline stuff, aka synchronize animations or something
 const tl = gsap.timeline({defaults: {duration: 1}})
 tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x: 1, y: 1})
 tl.fromTo('nav', {y: "-100%"}, {y:"0%"})
 tl.fromTo('.title', {opacity:0}, {opacity:1})
+
 
 //Mouse Animation Color
 let mouseDown = false
@@ -82,9 +96,10 @@ let rgb = [12, 23, 55];
 window.addEventListener('mousedown', () => (mouseDown = true))
 window.addEventListener('mouseup', () => (mouseDown = false))
 
+
 window.addEventListener('mousemove', (e) => {
   if(mouseDown){
-    rgb = [Math.round((e.pageX / sizes.width) * 255), 
+    rgb = [Math.round((e.pageX / sizes.width) * 255),
     Math.round((e.pageY / sizes.height) * 255), 150]
   }
   //Animate colors
